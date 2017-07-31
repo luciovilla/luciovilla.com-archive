4370a246f3b83aebc2d060664416d9ea3a124e42
Lucio Villa <lucio310@gmail.com>
2017-07-31 00:21:48 -0700

    updated grunt site task

--- tasks/buildSite.js
+++ tasks/buildSite.js
@@ -9,10 +9,10 @@ var shell = require("shelljs");
 module.exports = function(grunt) {
 
   grunt.registerTask("site", "Builds site files", function() {
-    if (grunt.file.exists("src/assets")) {
+    	shell.rm("-rf", "docs");
       shell.cp("-r", "build", "docs");
       shell.cp("-r", "CNAME", "docs");
-    }
+   
   });
 
 }
